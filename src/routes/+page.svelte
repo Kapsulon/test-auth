7f385a9bb2d1e8741f0ede234d550fda76b84fcc
Kapsulon <theo.bary@epitech.eu>
2023-11-30 08:31:55 +0100

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import { signIn } from "@auth/sveltekit/client"
+
+    let email: string = "";
+    let password: string = "";
+</script>
+
+<label for="email">Email:</label>
+<input type="email" id="email" bind:value={email} />
+<label for="password">Password:</label>
+<input type="password" id="password" bind:value={password} />
+<button on:click={() => {signIn("credentials", {
+    email,
+    password
+})}}>login</button>
